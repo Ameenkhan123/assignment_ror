113d946dc934086f4904b6419e97cebe02be965e
ameenkhan123 <ameenkhana4@gmail.com>
2019-10-26 12:49:31 +0530

    Generate and customize managers controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :managers
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # Devise Routes
+  devise_for :managers, controllers: {
+    sessions: 'managers/sessions',
+    registrations: 'managers/registrations'
+  }
 end
